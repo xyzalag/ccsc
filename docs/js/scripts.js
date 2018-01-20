a766d02a52409e3cb3467c08b7149caed203be6d
Aga <agnieszkagebus@gmail.com>
2018-01-20 19:22:30 +0100

    homepage v.1.0

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -59,33 +59,182 @@ jQuery(function ($) {
         if ($('#googleMap').length > 0) {
 
             //set your google maps parameters
-            var $latitude  = 48.869319, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-                $longitude = 2.354261,
-                $map_zoom  = 18;
+            var $latitude  = 50.061958, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+                $longitude = 19.919863,
+                $map_zoom  = 16;
             /* ZOOM SETTING */
 
             //google map custom marker icon
             var $marker_url = 'img/google-map-marker.png';
 
             //we define here the style of the map
-            var style = [{
-                "stylers" : [{
-                    "hue" : "#000"
-                }, {
-                    "saturation" : 100
-                }, {
-                    "gamma" : 1.15
-                }, {
-                    "lightness" : 5
-                }]
-            }];
+            var style = [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.icon",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#bdbdbd"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#ffffff"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dadada"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#c9c9c9"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              }
+            ];
 
             //set google map options
             var map_options = {
                 center            : new google.maps.LatLng($latitude, $longitude),
                 zoom              : $map_zoom,
                 panControl        : false,
-                zoomControl       : false,
+                zoomControl       : true,
                 mapTypeControl    : false,
                 streetViewControl : false,
                 mapTypeId         : google.maps.MapTypeId.ROADMAP,
@@ -104,4 +253,62 @@ jQuery(function ($) {
         }
     }());
 
+    // --------------------------------------------------------------------
+    // COUNTDOWN TIMER PLUGIN
+    // --------------------------------------------------------------------
+
+    (function () {
+        $("#getting-started").countdown("2018/05/12", function(event) {
+            $('#countdown-days').text(event.strftime('%D d'));
+            $('#countdown-hours').text(event.strftime('%H h '));
+            $('#countdown-minutes').text(event.strftime('%M m '));
+            $('#countdown-seconds').text(event.strftime('%S s '));
+
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // OWL CAROUSEL - PARTNERS
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            smartSpeed: 500,
+            autoplayHoverPause:true,
+            margin:20,
+            nav:false,
+            touchDrag: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
+    }());
+    // --------------------------------------------------------------------
+    // CONTACT FOOTER - SOCIAL ICONS HOVER
+    // --------------------------------------------------------------------
+
+    (function() {
+        $('.contact-social-hover').mouseenter(function() {
+          $(this).css({'background-color':'#000'});
+          $(this).find('a').css({'color':'#fff'});
+          $(this).find('a').addClass('neon');
+        });
+        $('.contact-social-hover').mouseleave(function() {
+          $(this).css({'background-color':'transparent'});
+          $(this).find('a').css({'color':'#000'});
+          $(this).find('a').removeClass('neon');
+        });
+    }());
+    
 }); // JQuery end
